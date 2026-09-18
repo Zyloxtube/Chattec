0c256337e9bcdb25c259af7feda783166e628cc7
ZyloxTube <tjadevofficial@gmail.com>
2026-09-18 11:37:05 +0300

    Customize root view controller in AppDelegate
    Updated the root view controller to customize layout margins and background color.

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -8,7 +8,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: RootView())
+
+        let host = UIHostingController(rootView: RootView())
+        // Let SwiftUI views extend under the status bar & home indicator
+        host.view.insetsLayoutMarginsFromSafeArea = false
+        host.view.backgroundColor = UIColor(red: 0x0f/255.0,
+                                            green: 0x13/255.0,
+                                            blue: 0x19/255.0,
+                                            alpha: 1)
+
+        window.rootViewController = host
         window.makeKeyAndVisible()
         self.window = window
         return true
